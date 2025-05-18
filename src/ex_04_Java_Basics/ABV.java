37c03db490d8da448366e511d52311bc1982436c
hussainobulampalle <hussain.obulampalle@builder.ai>
2025-05-18 11:21:03 +0530

    18th May 2025

--- src/ex_04_Java_Basics/ABV.java
+++ src/ex_04_Java_Basics/ABV.java
@@ -1,5 +1,11 @@
 package ex_04_Java_Basics;
 
 public class ABV {
+    public static void main(String[] args) {
+        int i = 11;
 
+        i = i++ + ++i;
+
+        System.out.println(i);
+    }
 }
