12c584b5fc2767aeab8513c14b4f74d5616f3a6a
hussainobulampalle <hussain.obulampalle@builder.ai>
2025-05-27 17:56:13 +0530

    25th May 2025

---
+++ src/ex_13_Functions/Lab128_Functions.java
@@ -0,0 +1,12 @@
+package ex_13_Functions;
+
+import java.net.StandardSocketOptions;
+
+public class Lab128_Functions {
+    public static void main(String[] args) {
+        //Built-im
+        //created by the java guys.
+        int result=Math.min(3,4);
+        System.out.println(result);
+    }
+}
---
+++ src/ex_13_Functions/Lab129_Method_Functions.java
@@ -0,0 +1,13 @@
+package ex_13_Functions;
+
+public class Lab129_Method_Functions {
+    public static void main(String[] args) {
+        //step 2 -Call
+        name_of_the_func();
+
+    }
+    //step 1 -def
+    public static void name_of_the_func() {
+        System.out.println("Hello World");
+    }
+}
